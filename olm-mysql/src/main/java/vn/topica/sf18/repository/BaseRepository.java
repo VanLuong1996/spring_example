c8f1b22a92ca13a2d9a6c4bd29975e9c356d2fdb
Nguyễn Quang Huy <quanghuy.ico@gmail.com>
2018-07-03 09:16:35 +0700

    seem ok

--- olm-mysql/src/main/java/vn/topica/sf18/repository/BaseRepository.java
+++ olm-mysql/src/main/java/vn/topica/sf18/repository/BaseRepository.java
@@ -2,11 +2,13 @@ package vn.topica.sf18.repository;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
+@NoRepositoryBean
 public interface BaseRepository<T, X> extends PagingAndSortingRepository<T, X>,
         JpaSpecificationExecutor<T> {
 
