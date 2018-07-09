74ba0289142a0014a9668d27953c3da33261a6fe
huynq12 <huynq12@topica.edu.vn>
2018-07-09 18:46:08 +0700

    fix rabbitmq

--- support/olm-queue-rabbitmq/src/main/java/vn/topica/sf18/queue/rabbitmq/config/EnableRabbitmqQueue.java
+++ support/olm-queue-rabbitmq/src/main/java/vn/topica/sf18/queue/rabbitmq/config/EnableRabbitmqQueue.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.context.annotation.Import;
 
-@Import({ModuleConfiguration.class})
+@Import({ModuleConfiguration.class, RabbitmqConfiguration.class})
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface EnableRabbitmqQueue {
